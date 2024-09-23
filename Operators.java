c46eaf807f91e65d0c63c51c95e82edbc02c1fe1
Suraj Rana <jp98064scoot@gmail.com>
2024-09-23 20:02:10 +0545

    Second commit for operators

--- Operators.java
+++ Operators.java
@@ -30,5 +30,26 @@ public class Operators {
         System.out.println("Equals to:"+Var2);
         boolean Var3=30!=10;
         System.out.println("Not equals operator:"+Var3);
+
+
+        //Logical Operators
+        //&&,||,!
+        //Used between two boolean stsement
+        boolean Log1=true&&false;
+        System.out.println("AND gate:"+Log1);
+        boolean AndExtended=(10>12)&&(20<30);
+        System.out.println("Numerical AND:"+AndExtended);
+        int logicVar1=20, logicVar2 = 30, logicVar3 = 30;
+        boolean orlogic = (logicVar1== logicVar3) || (logicVar2 < logicVar1) || (logicVar1 == logicVar3);
+        System.out.println("OR Gate:"+orlogic);
+        boolean notlogic = !(10==10);
+        System.out.println("NOT Gate:"+notlogic);
+
+
+        // unary
+        int intExample = 10 ;
+        intExample --; //intExample
+        intExample ++; //intExample
+        System.out.println("Unary Operators:"+intExample);
     }
 }
